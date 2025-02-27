a9d73037fab4e940ca4261dc10762d662ee74fb0
aleand8150 <andaliev.leza@gmail.com>
2025-02-27 11:02:04 +0300

    changed config and example env for TEST_DB_USER

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ DB_PASS = os.environ.get("DB_PASS")
 TEST_DB_HOST = os.environ.get("TEST_DB_HOST")
 TEST_DB_PORT = os.environ.get("TEST_DB_PORT")
 TEST_DB_NAME = os.environ.get("TEST_DB_NAME")
-TEST_DB_USER = "root"
+TEST_DB_USER = os.environ.get("TEST_DB_USER")
 TEST_DB_PASS = os.environ.get("TEST_DB_PASS")
 TEST_DB_URL = f"mysql://{TEST_DB_USER}:{TEST_DB_PASS}@{TEST_DB_HOST}:{TEST_DB_PORT}/{TEST_DB_NAME}"
 
